cfa43a9f119a325831be2a4b46b4de54062f814c
Kevin Jensen <jensenk28@yahoo.com>
2020-02-03 14:40:39 -0800

    building out registration form

--- picmetric/src/components/Register.js
+++ picmetric/src/components/Register.js
@@ -1,1 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
+
+export default class Registration extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            password_confirmation: '',
+            registrationErrors: ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}
+            </div>
+        );
+    }
+}
