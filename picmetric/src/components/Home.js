cea42d23523b806807783f3649d871333b563eea
Eian Carter <eian@bricleir.com>
2020-02-03 17:16:25 -0800

    changed home name

--- picmetric/src/components/Home.js
+++ picmetric/src/components/Home.js
@@ -9,7 +9,7 @@ export default class Home extends Component {
     render() {
         return (
             <div>
-                <h1>Welcome to PicMetric</h1>
+                <h1>Welcome</h1>
                 <h1>Status: {this.props.loggedInStatus}</h1>
                 <Register />
             </div>
