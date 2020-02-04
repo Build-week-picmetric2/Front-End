a84bd3b76437ab86045bae756cda1970d459db91
Eian Carter <eian@bricleir.com>
2020-02-04 14:40:42 -0800

    added photo upload form

--- picmetric/src/components/UploadPhoto.js
+++ picmetric/src/components/UploadPhoto.js
@@ -1,29 +1,56 @@
 import React, { useState } from 'react';
-import { axiosWithAuth } from '../helpers/axiosWithAuth';
+import axios from 'axios';
 
 const UploadPhoto = (props) => {
     const [ photo, setPhoto] = useState({
-        url: ''
+        preview: '',
+        raw: ''
     })
     const [ uploading, setUploading ] = useState(false)
 
-    const handleSubmit = (e) => {
-        axiosWithAuth()
-            .post('/api/photos', photo)
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData()
+        formData.append('photo', photo.raw)
+        const config = { headers: {'content-type': 'multipart/form-data' }}
+        try {
+            await axios
+            .post('/api/photos', {photo: photo.raw}, config)
+        } catch (err) {
+            console.log(err.res)
+        }
     }
     const handleChange = (e) => {
         setUploading(false)
         setPhoto({
-            url: ''
+            preview: URL.createObjectURL(e.target.files[0]),
+            raw: e.target.files[0]
         })
     }
     return(
         <div>
             <h1>Upload </h1>
+            <label htmlFor='upload-button'>
+                {
+                    photo.preview ? <img src={ photo.preview } width='300' height='250' alt='preview' /> : (
+                        <>
+                            {/* <span className="fa-stack fa-2x mt-3 mb-2">
+                                <i className="fas fa-circle fa-stack-2x"></i>
+                                <i className="fas fa-store fa-stack-1x fa-inverse"></i>
+                            </span> */}
+                            <h5 className="text-center"> Upload your photo</h5>
+                        </>
+                    )
+                }
+            </label>
             <input 
+                type='file'
+                id='upload-button'
+                style= {{ display: 'none' }}
                 onChange={handleChange}
             />
-            <button>Add Photo</button>
+            <br />
+            <button onClick={handleSubmit}>Add Photo</button>
         </div>
     )
 }
